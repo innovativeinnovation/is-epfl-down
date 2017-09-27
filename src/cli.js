472aac55aedf14ebb8b7bcaf32fcc8454d95105d
William Belle <william.belle@gmail.com>
2017-09-27 14:15:05 +0200

    version() is now enabled by default in yargs

--- src/cli.js
+++ src/cli.js
@@ -64,10 +64,6 @@ var yargs    = require('yargs')
 
   // Version
   .alias('v', 'version')
-  .version(function() {
-    return require('../package').version;
-  })
-  .describe('v', 'Show version information')
 
   // Help
   .help('h')
